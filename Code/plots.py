75bec6e8d61c746c5a4c8785ea2fac27bf1f0d7e
T-800 <adequin.renaud@gmail.com>
2017-01-06 21:18:03 +0100

    (tout) debut du tracking ...

--- Code/plots.py
+++ Code/plots.py
@@ -1,67 +1,35 @@
 import matplotlib.pyplot as plt
-import numpy as np
+
 import math
-
-'''
-bruits_0 = open("./bruit0.txt", "r")
-bruits_10 = open("./bruit10.txt", "r")
-bruits_25 = open("./bruit25.txt", "r")
-bruits_50 = open("./bruit50.txt", "r")
-
-END = 300
-X = []
-y_bruit0 = []
-y_bruit10 = []
-y_bruit25 = []
-y_bruit50 = []
-
-lines = bruits_0.readlines()
-for l in lines[:END]:
-	ll = l.split("\t")
-	X += [float(ll[0])]
-	y_bruit0 += [float(ll[1])]
-
-lines = bruits_10.readlines()
-for l in lines[:END]:
-	ll = l.split("\t")
-	y_bruit10 += [float(ll[1])]
-
-lines = bruits_25.readlines()
-for l in lines[:END]:
-	ll = l.split("\t")
-	y_bruit25 += [float(ll[1])]
-
-lines = bruits_50.readlines()
-for l in lines[:END]:
-	ll = l.split("\t")
-	y_bruit50 += [float(ll[1])]
-
-# plt.suptitle('Bruit sur une population de winner-comparator', fontsize=14, fontweight='bold')
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xlabel('temps')
-ax.set_ylabel('effort')
-ax.plot(X, y_bruit0, 'g-', label="0%")
-ax.plot(X, y_bruit10, 'r-', label="10%")
-ax.plot(X, y_bruit25, 'b-', label="25%")
-ax.plot(X, y_bruit50, 'y-', label="50%")
-plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
-
-plt.show()
-'''
 
 def do_plot(q1, q2, t, qd1, qd2):
 	fig2 = plt.figure()
 	fig2.suptitle('Variation des angles en fonction du temps.', fontsize=14, fontweight='bold')
 	ax2 = fig2.add_subplot(111, xlim=(0, 10))
-	#s1 = 'th1 = '+str(int(math.degrees(q1[0])))+' qd1 =  '+str(int(math.degrees(qd1)))
 	s2 = 'th2 = '+str(int(math.degrees(q2[0])))+' qd2 = '+str(int(math.degrees(qd2)))
 	ax2.set_title(s2)
 	ax2.grid()
-	ax2.set_xlabel('Temps')
+	ax2.set_xlabel('Temps (sec)')
 	ax2.set_ylabel('Angle (rad)')
 	ax2.plot(t, q1, 'g-', label="q1 - qd1")
 	ax2.plot(t, q2, 'r-', label="q2 - qd2")
 	plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
 	plt.savefig('Data/plots/plot_'+str(s2)+'.png', bbox_inches='tight')
 	plt.close(fig2)
+
+
+def do_plot_tracking(cmd, q2, t):
+	fig2 = plt.figure()
+	fig2.suptitle('Suivi de trajectoire', fontsize=14, fontweight='bold')
+	ax2 = fig2.add_subplot(111, xlim=(0, 40))
+	#s2 = 'th2 = '+str(int(math.degrees(q2[0])))+' qd2 = '+str(int(math.degrees(qd2)))
+	#ax2.set_title(s2)
+	ax2.grid()
+	ax2.set_xlabel('Temps (sec)')
+	ax2.set_ylabel('q2 (rad)')
+	ax2.plot(t, cmd, 'k--', label="commande")
+	ax2.plot(t, q2, 'k-', label="output")
+	plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
+	plt.savefig('Data/plots/plot_tracking.png', bbox_inches='tight')
+	plt.close(fig2)
+	print("sds")
